2c9b89c4c2a49807e018e1f2f3919818530338a8
GUIOVANNI HERNANDEZ MUÑOZ <guiovannihm@hotmail.com>
2024-11-05 05:59:39 -0500

    051124PT

--- ClassINVENTARIOS.vb
+++ ClassINVENTARIOS.vb
@@ -15,7 +15,7 @@ Public Class ClassINVENTARIOS
         _fr = Panelfr
         fr = New ClassConstructor22(_fr)
         dsim.campostb = "kimagen-key,nombre-varchar(250),foto-image"
-        dspi.campostb = "kproducto-key,referencia-varchar(250),diseno-varchar(250),marca-varchar(250),descripcion-varchar(250),precio_contado-money,precio_credito-money,grupo-varchar(250),bodega-varchar(250)"
+        dspi.campostb = "kproducto-key,referencia-varchar(250),diseno-varchar(250),marca-varchar(250),descripcion-varchar(250),precio_contado-money,precio_credito-money,grupo-varchar(250),bodega-varchar(250),disponible-bigint"
         Select Case fr.reque("fr")
             Case "INVENTARIO"
                 carga_inventario()
@@ -25,23 +25,23 @@ Public Class ClassINVENTARIOS
     End Sub
     Private Sub carga_inventario()
         fr.FORMULARIO("INVENTARIO", "TxBUSCAR,BtBUSCAR", True)
-        fr.FR_BOTONES("PRODUCTO",, True)
+        fr.FR_BOTONES("NUEVA_PLANTILLA,NUEVA_BODEGA,NUEVO_PRODUCTO")
+
         If lg.perfil > 0 Then
-            fr.FR_CONTROL("BtGUARDAR", evento:=AddressOf nuevo_pr) = "NUEVO PRODUCTO"
+            fr.FR_CONTROL("BtNUEVO_PRODUCTO", evento:=AddressOf nuevo_pr) = Nothing
+            fr.FR_CONTROL("BtNUEVA_PLANTILLA", evento:=AddressOf nueva_plantilla) = Nothing
+            fr.FR_CONTROL("BtNUEVA_BODEGA", evento:=AddressOf nueva_bodega) = Nothing
+            fr.FR_CONTROL("BtGUARDAR") = "INGRESO PRODUCTO"
         Else
             fr.FR_CONTROL("BtGUARDAR", False) = "NUEVO PRODUCTO"
         End If
-
-        'Pn1.BorderStyle = BorderStyle.Solid
-        'Pn1.Height = Unit.Pixel(300) ': Pn1.Width = Unit.Percentage(10)
-        'Pn2.BorderStyle = BorderStyle.Solid
-        'Pn2.Height = Unit.Pixel(300) ': Pn2.Width = Unit.Percentage(50)
         Dim FRP As Panel = _fr.FindControl("PnBOTONES")
         Dim TbINV As New Table : TbINV.Width = Unit.Percentage(100)
         Dim TbROW As New TableRow
         If fr.movil = False Then
             Dim TbCELL1 As New TableCell
             TbCELL1.Width = Unit.Percentage(20)
+            TbCELL1.VerticalAlign = VerticalAlign.Top
             TbCELL1.Controls.Add(Pn1)
             Dim TbCELL2 As New TableCell
             TbCELL2.Controls.Add(Pn2)
@@ -52,15 +52,23 @@ Public Class ClassINVENTARIOS
         FRP.Controls.Add(TbINV)
         carga_clases()
     End Sub
-
+    Dim frp As New ClassConstructor22(Pn2)
+    Dim frg As New ClassConstructor22(Pn2)
     Private Sub nuevo_pr()
-        Dim frp As New ClassConstructor22(Pn2)
         Dim CPLANTILLA As String = Nothing
         For Each ROW As DataRow In dspa.Carga_tablas("formulario='INVENTARIO' and criterio='LLANTA'").Rows
             CPLANTILLA += ",Tx" + ROW.Item("valor")
         Next
         frp.FORMULARIO("NUEVO PRODUCTO", "DrTIPO,DrGRUPO,DrBODEGA,TxREFERENCIA,TxDISEÑO,TxMARCA" + CPLANTILLA, True)
-
+    End Sub
+    Private Sub nueva_plantilla()
+        frp.FORMULARIO("NUEVA PLANTILLA", "TxNOMBRE", True)
+        Dim GrPL As New GridView
+        frp.CARGAD_GR(GrPL, dspa.Carga_tablas())
+        Pn2.Controls.Add(GrPL)
+    End Sub
+    Private Sub nueva_bodega()
+        frp.FORMULARIO("NUEVA BODEGA", "TxNOMBRE", True)
     End Sub
     Private Sub carga_clases()
 
